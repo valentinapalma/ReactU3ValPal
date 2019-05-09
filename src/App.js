d154dba9a94553695c5fe4a8e093df8991db46c0
valentinapalma <valentinapalma@hotmail.se>
2019-05-09 11:48:49 +0200

    routing fixad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import NavBarComponent from './components/NavBarComponent';
+import DashboardScreen from './screens/DashboardScreen';
+import UserScreen from './screens/UserScreen';
+import LoginScreen from './screens/LoginScreen';
+
+import './App.css';
 
 /* Mother of components. 
 Renderar navigationskomponenten som i sin tur renderar andra komponenter. */
@@ -8,7 +14,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBarComponent/>
+        <Router>
+          <NavBarComponent/>
+
+          <Route exact path="/" component={LoginScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/dashboard" component={DashboardScreen} />
+          <Route exact path="/user" component={UserScreen} />
+          <Route exact path="/user/:id" component={UserScreen} />
+        </Router>
       </div>
     );
   }
--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import DashboardScreen from '../screens/DashboardScreen';
-import UserScreen from '../screens/UserScreen';
-import LoginScreen from '../screens/LoginScreen';
 import styles from './ModuleStyling.module.css';
 
 /*
@@ -13,7 +10,6 @@ Dashboard, Login och User
 class NavBarComponent extends Component {
     render() {
         return (
-            <Router>
                 <div className={styles.navbar}>
                     <ul className={styles.navList}>
                         <NavLink className={styles.links} activeClassName={styles.active} to="/login">Login</NavLink>
@@ -21,13 +17,6 @@ class NavBarComponent extends Component {
                         <NavLink className={styles.links} activeClassName={styles.active} to="/user">User</NavLink>
                     </ul>
                 </div>
-
-                <Route exact path="/" component={LoginScreen} />
-                <Route exact path="/login" component={LoginScreen} />
-                <Route exact path="/dashboard" component={DashboardScreen} />
-                <Route exact path="/user" component={UserScreen} />
-                <Route exact path="/user/:id" component={UserScreen} />
-            </Router>
         )
     }
 }
